dee52dde8273bc071fef8ee4a15d98abf47fbdf9
Suraj Kumar <54070241+Suraj7757@users.noreply.github.com>
2026-03-22 22:50:31 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,4 @@
 import Sells from "./pages/Sells";
-// ... other imports
 
-function ProtectedRoutes() {
-  // ...
-  return (
-    <Routes>
-      <Route path="/" element={<Dashboard />} />
-      <Route path="/jobs" element={<RepairJobs />} />
-      <Route path="/customers" element={<Customers />} />
-      <Route path="/payments" element={<Payments />} />
-      <Route path="/settlements" element={<Settlements />} />
-      <Route path="/inventory" element={<Inventory />} />
-      <Route path="/sells" element={<Sells />} />
-      <Route path="/reports" element={<Reports />} />
-      <Route path="/settings" element={<SettingsPage />} />
-      <Route path="/trash" element={<Trash />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  );
-}
+<Route path="/sells" element={<Sells />} />
+<Route path="/jobs" element={<RepairJobs />} />
